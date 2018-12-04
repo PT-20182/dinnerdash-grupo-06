e60fcb110b1adf9b8da1a791e3006e7e2b5cc9aa
davi <davivasconcelos@cjr.org.br>
2018-12-04 17:41:34 -0200

    meal com imagem

--- db/migrate/20181130114743_create_meals.rb
+++ db/migrate/20181130114743_create_meals.rb
@@ -4,7 +4,6 @@ class CreateMeals < ActiveRecord::Migration[5.2]
       t.string :name
       t.string :description
       t.float :price
-      t.binary :image
       t.string :available
       t.string :category
       t.references :category, foreign_key: true
