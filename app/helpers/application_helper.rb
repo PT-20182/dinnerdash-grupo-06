6f86b8bd719fa6cf1f5fe2a6d51aa0d86e667d7d
davi <davivasconcelos@cjr.org.br>
2018-12-11 20:52:00 -0200

    order sem user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,20 @@
 module ApplicationHelper
 
 	def current_order
-		if !session[:order_id].nil?
-			Order.find(session[:order_id])
+		if !current_user.nil?
+		  	if !current_user.orders.last.nil?
+		  		return current_user.orders.last
+		  	# elsif !session[:order_id].nil?
+		  	# 	current_user.orders.last=Order.find(session[:order_id])
+		  	else 
+				Order.create(user_id: current_user.id)
+			end
 		else
-			Order.new
+			if !session[:order_id].nil?
+				Order.find(session[:order_id])
+			else
+				Order.new
+			end
 		end
 	end
 end
