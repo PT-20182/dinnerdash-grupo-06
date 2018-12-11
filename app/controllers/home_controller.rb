ddd7eab44b9488614b641c8af4cd12def9795ec2
larissa <larissaandrade@cjr.org.br>
2018-12-11 09:42:10 -0200

    pagina inicial mais arrumada

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
     def index 
         @categories = Category.all
         @meals = Meal.all
+        @order = current_order.order_items
         @order_item = current_order.order_items.new
     end
     def show
