1eb0062a5b60e77f681988614dc963cf01cea8d2
davi <davivasconcelos@cjr.org.br>
2018-12-12 17:11:53 -0200

    finalizar estranho

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,8 +2,8 @@ class OrdersController < ApplicationController
   def finish
     @categories = Category.all
     @meals = Meal.all
-    @order = current_order.order_items
-    @order_item = current_order.order_items.new
+    @order = current_user.orders
+    @order_item = current_order.order_items
   end
 
   def last
