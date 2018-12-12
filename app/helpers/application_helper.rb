9e5cf983db6f8668d68fbe50bf0c7e79493f4d92
davi <davivasconcelos@cjr.org.br>
2018-12-12 01:47:47 -0200

    user e order

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,20 +1,35 @@
 module ApplicationHelper
 
 	def current_order
-		if !current_user.nil?
-		  	if !current_user.orders.last.nil?
-		  		return current_user.orders.last
-		  	# elsif !session[:order_id].nil?
-		  	# 	current_user.orders.last=Order.find(session[:order_id])
-		  	else 
-				Order.create(user_id: current_user.id)
-			end
-		else
+		# if !current_user.nil?
+		#   	if !current_user.orders.last.nil?
+		#   		return current_user.orders.last
+		  	
+		#   	else 
+		# 		Order.create(user_id: current_user.id)
+		# 	end
+		# else
+			
 			if !session[:order_id].nil?
 				Order.find(session[:order_id])
 			else
 				Order.new
 			end
-		end
+			# end
+	end
+		
+			def change_order
+				ord=Order.new
+				session[:order_id]=ord.id
+				root_path
+			end
+		
+	def final_order
+		order=current_order
+		user=current_user
+		order.user_id=user.id
+		order.save!
+		change_order
+		root_path
 	end
 end
