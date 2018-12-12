69b98c5a0a0d98629225a4193dbedab030745cd2
PedroFernandeSpark <pedrofernandes@cjr.org.br>
2018-12-12 14:08:56 -0200

    tabela e tela de ultimo pedido (so nao funciona o ver itens pedidos)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,8 +4,17 @@ class OrdersController < ApplicationController
     @meals = Meal.all
     @order = current_order.order_items
     @order_item = current_order.order_items.new
+    if !current_user.nil?
+      @orders = current_user.orders
+    end
   end
 
   def last
+    @categories = Category.all
+    @meals = Meal.all
+    @order = current_order.order_items
+    if !current_user.nil?
+      @orders = current_user.orders
+    end
   end
 end
