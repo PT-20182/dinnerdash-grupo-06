598c61804cad1ce98db22c747955662586e085b9
JLYamin <erwn36@gmail.com>
2018-12-13 09:23:58 -0200

    Revert "Feature 17"

--- app/models/user.rb
+++
@@ -1,8 +0,0 @@
-class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :orders
-  # has_one :order
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-end
